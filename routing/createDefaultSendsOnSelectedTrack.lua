8428b66c22eadfff8bbe66367778e3731a047a61
Dusan Zatkovsky <dusanmsk@gmail.com>
2024-04-02 11:14:24 +0200

    Update createDefaultSendsOnSelectedTrack.lua
    reformat

--- routing/createDefaultSendsOnSelectedTrack.lua
+++ routing/createDefaultSendsOnSelectedTrack.lua
@@ -18,18 +18,18 @@ end
 
 selected_track = reaper.GetSelectedTrack(0, 0)
 if selected_track ~= nil then
-_, selected_track_name = reaper.GetSetMediaTrackInfo_String(selected_track, "P_NAME", "", false)
-if string.match(selected_track_name, "^SEND -") then
-  reaper.ShowMessageBox("Can't create routes to from 'SEND -' track itself", "Error",  0) 
-  return
-end
-track_count = reaper.CountTracks(0)
-for i = 0, track_count - 1 do
-    track = reaper.GetTrack(0, i)
-    _, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
-    if string.match(track_name, "^SEND -") and not isRoutedTo(selected_track, track) then
-        sendIdx = reaper.CreateTrackSend(selected_track, track)
-        reaper.SetTrackSendInfo_Value(selected_track, 0, sendIdx, "D_VOL", 0)
-    end
-end
+  _, selected_track_name = reaper.GetSetMediaTrackInfo_String(selected_track, "P_NAME", "", false)
+  if string.match(selected_track_name, "^SEND -") then
+    reaper.ShowMessageBox("Can't create routes to from 'SEND -' track itself", "Error",  0) 
+    return
+  end
+  track_count = reaper.CountTracks(0)
+  for i = 0, track_count - 1 do
+      track = reaper.GetTrack(0, i)
+      _, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
+      if string.match(track_name, "^SEND -") and not isRoutedTo(selected_track, track) then
+          sendIdx = reaper.CreateTrackSend(selected_track, track)
+          reaper.SetTrackSendInfo_Value(selected_track, 0, sendIdx, "D_VOL", 0)
+      end
+  end
 end
