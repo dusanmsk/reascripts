c618b701c1135be880fb63cf59febd87ebec481a
dusanmsk <msk.conf@gmail.com>
2024-04-02 11:11:50 +0200

    Update createDefaultSendsOnSelectedTrack.lua
    fine-tune

--- routing/createDefaultSendsOnSelectedTrack.lua
+++ routing/createDefaultSendsOnSelectedTrack.lua
@@ -6,32 +6,30 @@
 --   Default volume is -inf.
 
 function isRoutedTo(track1, track2)
-    local send_count = reaper.GetTrackNumSends(track1, 0)
-    for i = 0, send_count - 1 do
-      local dest_track = reaper.GetTrackSendInfo_Value(track1, 0, i, "P_DESTTRACK")
-      if dest_track == track2 then
-        return true
-      end
+  local send_count = reaper.GetTrackNumSends(track1, 0)
+  for i = 0, send_count - 1 do
+    local dest_track = reaper.GetTrackSendInfo_Value(track1, 0, i, "P_DESTTRACK")
+    if dest_track == track2 then
+      return true
     end
-    return false
+  end
+  return false
 end
 
 selected_track = reaper.GetSelectedTrack(0, 0)
+if selected_track ~= nil then
 _, selected_track_name = reaper.GetSetMediaTrackInfo_String(selected_track, "P_NAME", "", false)
 if string.match(selected_track_name, "^SEND -") then
-  reaper.ShowMessageBox("Error", "Can't create route to from 'SEND -' track to all 'SEND -' tracks", 0) 
+  reaper.ShowMessageBox("Can't create routes to from 'SEND -' track itself", "Error",  0) 
   return
 end
-
-if selected_track ~= nil then
-    track_count = reaper.CountTracks(0)
-    for i = 0, track_count - 1 do
-        track = reaper.GetTrack(0, i)
-        _, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
-        if string.match(track_name, "^SEND -") and not isRoutedTo(selected_track, track) then
-            sendIdx = reaper.CreateTrackSend(selected_track, track)
-            reaper.SetTrackSendInfo_Value(selected_track, 0, sendIdx, "D_VOL", 0)
-        end
+track_count = reaper.CountTracks(0)
+for i = 0, track_count - 1 do
+    track = reaper.GetTrack(0, i)
+    _, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
+    if string.match(track_name, "^SEND -") and not isRoutedTo(selected_track, track) then
+        sendIdx = reaper.CreateTrackSend(selected_track, track)
+        reaper.SetTrackSendInfo_Value(selected_track, 0, sendIdx, "D_VOL", 0)
     end
 end
-
+end
